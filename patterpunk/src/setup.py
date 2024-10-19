8d5f761d782442410f3929c1019cf023a32bc19b
tho <someone@example.com>
2024-10-19 13:50:38 -0700

    Update commands to compose plugin, prep for pypi release

--- patterpunk/src/setup.py
+++ patterpunk/src/setup.py
@@ -7,14 +7,24 @@ VERSION = "0.0.6"
 current_dir = Path(__file__).parent
 requirements_path = current_dir / "requirements.txt"
 
+
 setup(
     name="patterpunk",
     version=VERSION,
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests", "bin", "dist"]),
+    url="https://github.com/trb/patterpunk",
+    author="Thomas Rubbert",
+    author_email="thomas.rubbert@yahoo.de",
+    license="MPL-2.0",
     include_package_data=True,
     install_requires=[
         # This reads the requirements from the requirements.txt file
         line.strip()
         for line in open(requirements_path, "r")
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: Mozilla Public License 2.0",
+        "Intended Audience :: Developers",
     ],
 )
