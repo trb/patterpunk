6625ad26e14dc0ee58d8a32865ad6408d5c6f98e
tho <someone@example.com>
2024-11-26 14:00:30 -0800

    Release 0.0.16

--- patterpunk/src/setup.py
+++ patterpunk/src/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 from pathlib import Path
 
 
-VERSION = "0.0.15"
+VERSION = "0.0.16"
 
 current_dir = Path(__file__).parent
 requirements_path = current_dir / "requirements.txt"
