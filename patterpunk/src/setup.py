28bc2ca8f3c24573e6ccbb6c837e17ffece971b4
tho <someone@example.com>
2025-04-16 21:18:49 -0700

    Add release artefact

--- patterpunk/src/setup.py
+++ patterpunk/src/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 from pathlib import Path
 
 
-VERSION = "0.0.18"
+VERSION = "0.0.19"
 
 current_dir = Path(__file__).parent
 requirements_path = current_dir / "requirements.txt"
