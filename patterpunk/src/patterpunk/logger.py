25223c44227df43bf1e40f1ec2f896753d2b4d46
tho <someone@example.com>
2026-01-26 15:03:16 -0800

    Improve logging when used in apps

--- patterpunk/src/patterpunk/logger.py
+++ patterpunk/src/patterpunk/logger.py
@@ -1,15 +1,12 @@
 import logging
 
+# Libraries should only create loggers, not configure handlers.
+# The application controls how logs are formatted and where they go.
+# See: https://docs.python.org/3/howto/logging.html#configuring-logging-for-a-library
+
 logger = logging.getLogger("patterpunk")
 logger_llm = logging.getLogger("patterpunk.llm")
 
-logger.setLevel(logging.DEBUG)
-logger_llm.setLevel(logging.DEBUG)
-
-logger_handler = logging.StreamHandler()
-logger.addHandler(logger_handler)
-
-logger_llm_handler = logging.StreamHandler()
-logger_llm.addHandler(logger_llm_handler)
-
-logger_llm.propagate = False
+# Add NullHandler to prevent "No handler found" warnings when
+# the application doesn't configure logging for this namespace.
+logger.addHandler(logging.NullHandler())
